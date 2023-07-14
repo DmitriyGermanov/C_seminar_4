a8dba36dfb4a552c5c6c99f5d0766808211c98fb
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-14 21:02:37 +0300

    First and Second task

---
+++ task00/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetNumber(string message)
+{
+    Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void GetSumNumber(int a)
+{
+    int summ = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        summ += i;
+    }
+
+    Console.WriteLine($"Сумма чисел от 1 до {a} = {summ}");
+}
+
+int num = GetNumber("Введите число:");
+GetSumNumber(num);
+
+
---
+++ task01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Решение в группах задач:
+// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+int GetNumber(string message)
+{
+    Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int FindRazryad(int findRazryad) // Функция нахождения разряда
+{
+    int razryadNumber = 0;
+    while (findRazryad > 0)
+    {
+        findRazryad = findRazryad / 10;
+        razryadNumber++;
+    }
+    return razryadNumber;
+}
+int num = GetNumber("Введите число:");
+Console.WriteLine(FindRazryad(num));
