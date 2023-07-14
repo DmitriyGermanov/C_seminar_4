ede379d85847d2a581910fce19e4420f07fee1e6
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-14 21:20:25 +0300

    added second task

---
+++ task02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int GetNumber(string message)
+{
+    Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Multiplication(int a)
+{
+    int b = 1;
+    for (int i = 1; i <= a; i++)
+        b = b * i;
+    return b;
+}
+
+int num = GetNumber("Введите число:");
+int mult = Multiplication(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} = {mult}");
