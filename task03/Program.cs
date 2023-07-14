da988db84020c2985b891bf4ec0324f22e69a2a6
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-14 22:19:40 +0300

    Added task 3

---
+++ task03/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 30: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int Promt(string message) // создаем метод для печати
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int[] Fill(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(0, 2);
+    }
+    return a;
+}
+void PrintMass(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"{a[i]} ");
+    }
+}
+
+int N = Promt("Введите длину массива: ");
+int[] array = new int[N];
+PrintMass(Fill(array));
